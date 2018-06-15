f4c9efcf2df31622a04b62cad234dbda789cd564
timgaripov <timgaripov@gmail.com>
2018-06-16 01:28:17 +0300

    diag laplace approximation (needs some more testing)

---
+++ laplace.py
@@ -0,0 +1,96 @@
+import torch
+import numpy as np
+import torch.distributions
+import time
+
+
+def laplace_parameters(module, params):
+    for name in list(module._parameters.keys()):
+        if module._parameters[name] is None:
+            print(module, name)
+            continue
+        data = module._parameters[name].data
+        module._parameters.pop(name)
+        module.register_buffer('%s_mean' % name, data.new(data.size()).zero_())
+        module.register_buffer('%s_var' % name, data.new(data.size()).zero_())
+        module.register_buffer(name, data.new(data.size()).zero_())
+
+        params.append((module, name))
+
+
+class Laplace(torch.nn.Module):
+    def __init__(self, base, *args, **kwargs):
+        super(Laplace, self).__init__()
+
+        self.params = list()
+
+        self.base = base(*args, **kwargs)
+        self.base.apply(lambda module: laplace_parameters(module=module, params=self.params))
+
+    def forward(self, input):
+        return self.base(input)
+
+    def sample(self, scale=1.0, require_grad=False):
+        for module, name in self.params:
+            mean = module.__getattr__('%s_mean' % name)
+            var = module.__getattr__('%s_var' % name)
+            eps = mean.new(mean.size()).normal_()
+            w = mean + scale * torch.sqrt(var) * eps
+            if require_grad:
+                w.requires_grad_()
+            module.__setattr__(name, w)
+            getattr(module, name)
+
+    def export_numpy_params(self):
+        mean_list = []
+        var_list = []
+        for module, name in self.params:
+            mean_list.append(module.__getattr__('%s_mean' % name).cpu().numpy().ravel())
+            var_list.append(module.__getattr__('%s_var' % name).cpu().numpy().ravel())
+        mean = np.concatenate(mean_list)
+        var = np.concatenate(var_list)
+        return mean, var
+
+    def import_numpy_mean(self, w):
+        k = 0
+        for module, name in self.params:
+            mean = module.__getattr__('%s_mean' % name)
+            s = np.prod(mean.shape)
+            module.__setattr__(name, mean.new_tensor(w[k:k + s].reshape(mean.shape)))
+            k += s
+
+    def estimate_variance(self, loader, criterion, samples=40, tau=5e-4):
+        fisher_diag = dict()
+        for module, name in self.params:
+            var = module.__getattr__('%s_var' % name)
+            fisher_diag[(module, name)] = var.new(var.size()).zero_()
+        self.sample(scale=0.0, require_grad=True)
+        for s in range(samples):
+            t_s = time.time()
+            for input, target in loader:
+                input = input.cuda(async=True)
+                target = target.cuda(async=True)
+
+                output = self(input)
+                distribution = torch.distributions.Categorical(logits=output)
+                y = distribution.sample()
+                loss = criterion(output, y)
+
+                loss.backward()
+
+                bs = input.size(0)
+                for module, name in self.params:
+                    grad = module.__getattr__(name).grad
+                    fisher_diag[(module, name)].add_(torch.pow(grad, 2) * bs)
+            t = time.time() - t_s
+            print('%d/%d %.2f sec' % (s + 1, samples, t))
+
+        for module, name in self.params:
+            f = fisher_diag[(module, name)] / samples
+            var = 1.0 / (f  + tau)
+            module.__getattr__('%s_var' % name).copy_(var)
+
+
+
+
+
---
+++ swag_laplace.py
@@ -0,0 +1,58 @@
+import argparse
+import torch
+import models, swag, data, utils, laplace
+import torch.nn.functional as F
+import numpy as np
+
+parser = argparse.ArgumentParser(description='SGD/SWA training')
+parser.add_argument('--file', type=str, default=None, required=True, help='checkpoint')
+
+parser.add_argument('--dataset', type=str, default='CIFAR10', help='dataset name (default: CIFAR10)')
+parser.add_argument('--data_path', type=str, default='/scratch/datasets/', metavar='PATH',
+                    help='path to datasets location (default: None)')
+parser.add_argument('--use_test', dest='use_test', action='store_true', help='use test dataset instead of validation (default: False)')
+parser.add_argument('--batch_size', type=int, default=128, metavar='N', help='input batch size (default: 128)')
+parser.add_argument('--num_workers', type=int, default=4, metavar='N', help='number of workers (default: 4)')
+parser.add_argument('--model', type=str, default='VGG16', metavar='MODEL',
+                    help='model name (default: VGG16)')
+
+
+parser.add_argument('--seed', type=int, default=1, metavar='S', help='random seed (default: 1)')
+
+
+
+args = parser.parse_args()
+
+torch.backends.cudnn.benchmark = True
+torch.manual_seed(args.seed)
+torch.cuda.manual_seed(args.seed)
+
+print('Using model %s' % args.model)
+model_cfg = getattr(models, args.model)
+
+print('Loading dataset %s from %s' % (args.dataset, args.data_path))
+loaders, num_classes = data.loaders(
+    args.dataset,
+    args.data_path,
+    args.batch_size,
+    args.num_workers,
+    model_cfg.transform_train,
+    model_cfg.transform_test,
+    use_validation=not args.use_test
+)
+
+
+print('Preparing models')
+swag_model = swag.SWAG(model_cfg.base, *model_cfg.args, num_classes=num_classes, **model_cfg.kwargs)
+swag_model.cuda()
+laplace_model = laplace.Laplace(model_cfg.base, *model_cfg.args, num_classes=num_classes, **model_cfg.kwargs)
+laplace_model.cuda()
+
+print('Loading model %s' % args.file)
+checkpoint = torch.load(args.file)
+swag_model.load_state_dict(checkpoint['state_dict'])
+
+mean, var = swag_model.export_numpy_params()
+laplace_model.import_numpy_mean(mean)
+print('Estimating variance')
+laplace_model.estimate_variance(loaders['train'], F.cross_entropy)
