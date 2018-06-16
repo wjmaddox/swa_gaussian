a9e05b502dd38b829b5a48d60aac5ea379b3ab7f
timgaripov <timgaripov@gmail.com>
2018-06-16 21:23:53 +0300

    num_classes and merge fixes

--- data.py
+++ data.py
@@ -4,29 +4,22 @@ import torchvision
 import os
 import regression_data
 
-def loaders(dataset, path, batch_size, num_workers, transform_train, transform_test, use_validation=True, val_size=5000):
-    regression_problem = False
-    try:
-        ds = getattr(torchvision.datasets, dataset)
-    except:
-        if dataset=='toy_regression':
-            ds = regression_data.generate_toy_problem
-            regression_problem = True
-
-
 c10_classes = np.array([
     [0, 1, 2, 8, 9],
     [3, 4, 5, 6, 7]
 ], dtype=np.int32)
 
-def loaders(dataset, path, batch_size, num_workers, transform_train, transform_test, use_validation=True, split_classes=None):
-    ds = getattr(torchvision.datasets, dataset)
+def loaders(dataset, path, batch_size, num_workers, transform_train, transform_test, use_validation=True, val_size=5000, split_classes=None):
+
+    regression_problem = False
+    try:
+        ds = getattr(torchvision.datasets, dataset)
+    except:
+        if dataset == 'toy_regression':
+            ds = regression_data.generate_toy_problem
+            regression_problem = True
     path = os.path.join(path, dataset.lower())
     train_set = ds(root=path, train=True, download=True, transform=transform_train)
-
-    num_classes = max(train_set.train_labels) + 1
-    if regression_problem:
-        num_classes = 0
 
 
     if use_validation:
@@ -48,19 +41,23 @@ def loaders(dataset, path, batch_size, num_workers, transform_train, transform_t
         assert dataset == 'CIFAR10'
         assert split_classes in {0, 1}
 
-
         print('Using classes:', end='')
         print(c10_classes[split_classes])
         train_mask = np.isin(train_set.train_labels, c10_classes[split_classes])
         train_set.train_data = train_set.train_data[train_mask, :]
-        train_set.train_labels = np.array(train_set.train_labels)[train_mask].tolist()
+        train_set.train_labels = np.array(train_set.train_labels)[train_mask]
+        train_set.train_labels = np.where(train_set.train_labels[:, None] == c10_classes[split_classes][None, :])[1].tolist()
         print('Train: %d/%d' % (train_set.train_data.shape[0], train_mask.size))
 
         test_mask = np.isin(test_set.test_labels, c10_classes[split_classes])
         test_set.test_data = test_set.test_data[test_mask, :]
-        test_set.test_labels = np.array(test_set.test_labels)[test_mask].tolist()
+        test_set.test_labels = np.array(test_set.test_labels)[test_mask]
+        test_set.test_labels = np.where(test_set.test_labels[:, None] == c10_classes[split_classes][None, :])[1].tolist()
         print('Test: %d/%d' % (test_set.test_data.shape[0], test_mask.size))
 
+    num_classes = max(train_set.train_labels) + 1
+    if regression_problem:
+        num_classes = 0
 
     return \
         {
