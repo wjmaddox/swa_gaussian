bugfix scaling issue with swag cov + add bugfix check notebook

swag/posteriors/swag.py
@@ -39,7 +39,7 @@ def swag_parameters(module, params, no_cov_mat=True, num_models=0):
 
 
 class SWAG(torch.nn.Module):
-    def __init__(self, base, no_cov_mat = True, max_num_models = 0, loading = False, *args, **kwargs):
+    def __init__(self, base, no_cov_mat = True, max_num_models = 0, loading = False, var_clamp = 1e-30, *args, **kwargs):
         super(SWAG, self).__init__()
 
         self.register_buffer('n_models', torch.zeros([1]))
@@ -51,6 +51,8 @@ class SWAG(torch.nn.Module):
             num_models = self.max_num_models
         else:
             num_models = 0
+
+        self.var_clamp = var_clamp
 
         self.base = base(*args, **kwargs)
         self.base.apply(lambda module: swag_parameters(module=module, params=self.params, no_cov_mat=self.no_cov_mat, num_models=num_models))
@@ -72,29 +74,32 @@ class SWAG(torch.nn.Module):
             mean = module.__getattr__('%s_mean' % name)
 
             sq_mean = module.__getattr__('%s_sq_mean' % name)
-            eps = mean.new(mean.size()).normal_()
-            diag_sample = scale * torch.sqrt(torch.clamp(sq_mean - mean ** 2, min=0.)) * eps
-            #print( (sq_mean.double() - (mean.double() ** 2)).min() )
+            eps = torch.randn_like(mean)
+            
+            var = torch.clamp(sq_mean - mean ** 2, self.var_clamp)
+
+            scaled_diag_sample = scale * torch.sqrt(var) * eps
 
             if cov is True:
                 cov_mat_sqrt = module.__getattr__('%s_cov_mat_sqrt' % name)
-                eps = torch.zeros(cov_mat_sqrt.size(0), 1).normal_().cuda() #rank-deficient normal results
+                eps = cov_mat_sqrt.new_empty((cov_mat_sqrt.size(0), 1)).normal_()
                 cov_sample = (scale/((self.max_num_models - 1) ** 0.5)) * cov_mat_sqrt.t().matmul(eps).view_as(mean)
 
                 if fullrank:
-                    w = mean + diag_sample + cov_sample
+                    w = mean + scaled_diag_sample + cov_sample
                 else:
-                    w = mean + diag_sample 
+                    w = mean + scaled_diag_sample 
 
             else:                
-                w = mean + diag_sample
+                w = mean + scaled_diag_sample
 
             module.__setattr__(name, w)
 
     def sample_fullrank(self, scale, cov, fullrank):
         #different sampling procedure to prevent block-diagonal gaussians from being sampled
         if cov is True and scale != 0.0:
-            #combine all cov mats into a list
+            #combine all cov mats and means into a list
+            #TODO: change unflatten like to to allow for a list of sizes for decreased memory
             cov_mat_sqrt_list = []
             mean_list = []
             for module, name in self.params:
@@ -102,15 +107,16 @@ class SWAG(torch.nn.Module):
                 mean_list.append(mean_current)
 
                 cov_mat_sqrt_current = module.__getattr__('%s_cov_mat_sqrt' % name)
-                #cov_mat_sqrt_list.append(cov_mat_sqrt_current)
-                cov_mat_sqrt_list.append(cov_mat_sqrt_current.cpu())
+                cov_mat_sqrt_list.append(cov_mat_sqrt_current)
             
-            #now flatten the covariances into a matrix
+            # flatten the covariances into a matrix
             cov_mat_sqrt = torch.cat(cov_mat_sqrt_list,dim=1)
-            #eps = torch.zeros(cov_mat_sqrt.size(0), 1).normal_().cuda() #rank-deficient normal results
-            eps = torch.zeros(cov_mat_sqrt.size(0), 1).normal_() #rank-deficient normal results
+
+            # draw rank-deficient normal
+            eps = cov_mat_sqrt.new_empty((cov_mat_sqrt.size(0), 1)).normal_()
+            
+            # rescale by scale/sqrt(N-1) for scaling + variance
             zero_mean_samples = (scale/((self.max_num_models - 1) ** 0.5)) * cov_mat_sqrt.t().matmul(eps)
-            zero_mean_samples = zero_mean_samples.cuda()
 
             #unflatten the covariances back into a list
             zero_mean_samples_list = unflatten_like(zero_mean_samples.t(), mean_list)
@@ -123,26 +129,28 @@ class SWAG(torch.nn.Module):
 
         for (module, name), sample in iterator:                    
             mean = module.__getattr__('%s_mean' % name)
-#             sample = sample.cuda()
+
             if scale == 0.0:
                 w = mean
             else:
-                #print('here cov is', cov)
-                if cov is True:
-                    sq_mean = module.__getattr__('%s_sq_mean' % name)
-                    eps = mean.new(mean.size()).normal_()
+                sq_mean = module.__getattr__('%s_sq_mean' % name)
+                eps = torch.randn_like(mean)
+                
+                var = torch.clamp(sq_mean - mean ** 2, self.var_clamp)
 
+                scaled_diag_sample = scale * torch.sqrt(var) * eps
+
+                if cov is True:
                     if fullrank:
                         #see Section 3.3 of variational boosting
                         #Cov(D'z_1 + sigma I z_2) = DD' + sigma I
-                        w = mean + sample.view_as(mean) + torch.sqrt(sq_mean - mean ** 2) * eps
+                        w = mean + sample.view_as(mean) + scaled_diag_sample
                     else:
                         w = mean + sample.view_as(mean)
 
                 else:
-                    sq_mean = module.__getattr__('%s_sq_mean' % name)
-                    eps = mean.new(mean.size()).normal_()
-                    w = mean + scale * torch.sqrt(sq_mean - mean ** 2) * eps
+
+                    w = mean + scaled_diag_sample
             module.__setattr__(name, w)
 
     def collect_model(self, base_model):