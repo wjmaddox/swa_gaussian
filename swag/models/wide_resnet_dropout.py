372aa84dee643d918022b96e58aa29d595826fd8
Pavel Izmailov <pi49@tgpu-slurm.coecis.cornell.edu>
2019-01-19 05:37:15 -0500

    SGD ensembling

--- swag/models/wide_resnet_dropout.py
+++ swag/models/wide_resnet_dropout.py
@@ -98,12 +98,14 @@ class WideResNet28x10Drop:
     args = list()
     kwargs = {'depth': 28, 'widen_factor': 10}
     transform_train = transforms.Compose([
+        transforms.Resize(32),
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
     transform_test = transforms.Compose([
+        transforms.Resize(32),
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
