numeric fix for temp scaling

experiments/uncertainty/run_unc_metrics.py
@@ -4,6 +4,7 @@ import os
 os.sys.path.append("/home/izmailovpavel/Documents/Projects/private_swa_uncertainties/experiments/uncertainty")
 from temp_scaling import optimal_temp_scale, rescale_temp
 import numpy as np
+import pickle
 
 parser = argparse.ArgumentParser(description='SGD/SWA training')
 parser.add_argument('--path', type=str, default=None, required=True, help='dataset name')
@@ -79,9 +80,12 @@ def nll(npz_arr, name="", print_name=False):
     return nll
 
 
-def get_path(filename, append=""):
-    print(os.path.join(args.path, filename + append + ".npz"))
-    return os.path.join(args.path, filename + append + ".npz")
+def get_path(filename, append="", no_npz=False):
+    path = os.path.join(args.path, filename + append)
+    if not no_npz:
+        path += ".npz"
+    print(path)
+    return path
 
 
 def nonedict():
@@ -103,11 +107,16 @@ try: swa_drop = np.load(get_path("unc_swa_drop", append=args.append))
 except: swa_drop = nonedict(); print("failed")
 try: sgd_drop = np.load(get_path("unc_sgd_drop", append=args.append))
 except: sgd_drop = nonedict(); print("failed")
+try: laplace_sgd = np.load(get_path("unc_laplace", append=args.append))
+except: laplace_sgd = nonedict(); print("failed")
+try: sgd_ens = np.load(get_path("sgd_ens_preds", append=args.append))
+except: sgd_ens = nonedict(); print("failed")
 
 # Temperature scaling
-if swa_val["predictions"] is not None:
+if swa["predictions"] is not None and swa_val["predictions"] is not None:
     print("Rescaling SWA temp")
     T_swa, rescaled_swa = optimal_temp_scale(swa_val["predictions"], swa_val["targets"], max_iter=50, lr=1e-3)
+    print(T_swa)
     if np.isnan(T_swa):
         T_swa=1
     preds = rescale_temp(swa["predictions"], T_swa)
@@ -115,7 +124,7 @@ if swa_val["predictions"] is not None:
 else:
     swa_temp = nonedict()
 
-if sgd_val["predictions"] is not None:
+if sgd["predictions"] is not None and sgd_val["predictions"] is not None:
     print("Rescaling SGD temp")
     T_sgd, rescaled_sgd = optimal_temp_scale(sgd_val["predictions"], sgd_val["targets"], max_iter=50, lr=1e-3)
     if np.isnan(T_sgd):
@@ -130,12 +139,19 @@ methods = [(sgd, "SGD"), (swa, "SWA"),
            (swag_cov, "SWAG-Cov"), 
            (sgd_drop, "SGD-Drop"), (swa_drop, "SWA-Drop"), 
            (sgd_temp, "SGD-temp"), (swa_temp, "SWA-temp"), 
+           (laplace_sgd, "Laplace-SGD"),
+           (nonedict(), "Laplace-SWA"),
+           (sgd_ens, "SGD-Ens")
            ]
 
 metrics = [(accuracy, "Accuracy"), (ece, "ECE"), (nll, "NLL")]
+results = {name: {} for _, name in methods}
 
 for metric, metric_name in metrics:
     print(metric_name)
     for method, name in methods:
-        metric(method, name, print_name=args.print_names)
+        res = metric(method, name, print_name=args.print_names)
+        results[name][metric_name] = res
     print()
+with open(get_path("results.pkl", no_npz=True), 'wb') as f:
+    pickle.dump(results, f, pickle.HIGHEST_PROTOCOL)

experiments/uncertainty/temp_scaling.py
@@ -10,11 +10,10 @@ import numpy as np
 def logits_from_probs(prob_arr):
     return np.log(prob_arr)
 
-
 def optimal_temp_scale(probs_arr, labels_arr, lr=0.01, max_iter=50):
     probs = torch.from_numpy(probs_arr).float()
     labels = torch.from_numpy(labels_arr.astype(int))
-    logits = torch.log(probs)
+    logits = torch.log(probs + 1e-12)
     nll_criterion = nn.CrossEntropyLoss()
     
     before_temperature_nll = nll_criterion(logits, labels).item()
@@ -25,12 +24,13 @@ def optimal_temp_scale(probs_arr, labels_arr, lr=0.01, max_iter=50):
     optimizer = optim.LBFGS([T], lr=lr, max_iter=max_iter)
     def eval():
         loss = nll_criterion(logits / T, labels)
-        loss.backward()
+        loss.backward(retain_graph=True)
         return loss
+
     optimizer.step(eval)
     
     after_temperature_nll = nll_criterion(logits / T, labels).item()
-    print('After temperature - NLL: %.3f' % (after_temperature_nll))
+    print('After temperature - NLL: %.3f' % (after_temperature_nll), ", Temperature:", T)
     
     return T.item(), F.softmax(logits / T).data.numpy()
 