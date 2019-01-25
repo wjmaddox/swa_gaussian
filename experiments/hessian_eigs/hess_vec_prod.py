853d94e42699bab880096f7b1584106957b38179
Wesley Maddox <wm326@cornell.edu>
2019-01-25 14:24:54 -0500

    move unflatten_like to utils

--- experiments/hessian_eigs/hess_vec_prod.py
+++ experiments/hessian_eigs/hess_vec_prod.py
@@ -8,7 +8,10 @@ import time
 import numpy as np
 from torch import nn
 from torch.autograd import Variable
-from scipy.sparse.linalg import LinearOperator, eigsh
+#from scipy.sparse.linalg import LinearOperator, eigsh
+from gpytorch.utils.lanczos import lanczos_tridiag, lanczos_tridiag_to_diag
+
+from swag.utils import flatten, unflatten_like
 
 ################################################################################
 #                              Supporting Functions
@@ -43,6 +46,9 @@ def gradtensor_to_npvec(net, include_bn=False):
     filter = lambda p: include_bn or len(p.data.size()) > 1
     return np.concatenate([p.grad.data.cpu().numpy().ravel() for p in net.parameters() if filter(p)])
 
+def gradtensor_to_tensor(net, include_bn=False):
+    filter = lambda p: include_bn or len(p.data.size()) > 1
+    return flatten([p.grad.data for p in net.parameters() if filter(p)])
 
 ################################################################################
 #                  For computing Hessian-vector products
@@ -68,23 +74,27 @@ def eval_hess_vec_prod(vec, params, net, criterion, dataloader, use_cuda=False):
     net.zero_grad() # clears grad for every parameter in the net
 
     for batch_idx, (inputs, targets) in enumerate(dataloader):
-        inputs, targets = Variable(inputs), Variable(targets)
-        if use_cuda:
-            inputs, targets = inputs.cuda(), targets.cuda()
+        if batch_idx < 10:
+            inputs, targets = Variable(inputs), Variable(targets)
+            if use_cuda:
+                inputs, targets = inputs.cuda(), targets.cuda()
 
-        outputs = net(inputs)
-        loss = criterion(outputs, targets)
-        grad_f = torch.autograd.grad(loss, inputs=params, create_graph=True)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+            grad_f = torch.autograd.grad(loss, inputs=params, create_graph=True)
 
-        # Compute inner product of gradient with the direction vector
-        prod = Variable(torch.zeros(1)).type(type(grad_f[0].data))
-        for (g, v) in zip(grad_f, vec):
-            prod = prod + (g * v).cpu().sum()
+            # Compute inner product of gradient with the direction vector
+            #prod = Variable(torch.zeros(1)).type(type(grad_f[0].data))
+            prod = torch.zeros(1, dtype=grad_f[0].dtype, device = grad_f[0].device)
+            for (g, v) in zip(grad_f, vec):
+                prod = prod + (g * v).sum()
 
-        # Compute the Hessian-vector product, H*v
-        # prod.backward() computes dprod/dparams for every parameter in params and
-        # accumulate the gradients into the params.grad attributes
-        prod.backward()
+            # Compute the Hessian-vector product, H*v
+            # prod.backward() computes dprod/dparams for every parameter in params and
+            # accumulate the gradients into the params.grad attributes
+            prod.backward()
+        else:
+            continue
 
 
 ################################################################################
@@ -111,18 +121,25 @@ def min_max_hessian_eigs(net, dataloader, criterion, rank=0, use_cuda=False, ver
 
     def hess_vec_prod(vec):
         hess_vec_prod.count += 1  # simulates a static variable
-        vec = npvec_to_tensorlist(vec, params)
+        #print(vec, vec.dtype, vec.device)
+        #vec = npvec_to_tensorlist(vec, params)
+        vec = unflatten_like(vec, params)
+
         start_time = time.time()
         eval_hess_vec_prod(vec, params, net, criterion, dataloader, use_cuda)
         prod_time = time.time() - start_time
         if verbose and rank == 0: print("   Iter: %d  time: %f" % (hess_vec_prod.count, prod_time))
-        return gradtensor_to_npvec(net)
+        #return gradtensor_to_npvec(net)
+        return gradtensor_to_tensor(net)
 
     hess_vec_prod.count = 0
     if verbose and rank == 0: print("Rank %d: computing max eigenvalue" % rank)
 
-    A = LinearOperator((N, N), matvec=hess_vec_prod)
-    pos_eigvals, _ = eigsh(A, k=1, tol=1e-2)
+    #A = LinearOperator((N, N), matvec=hess_vec_prod)
+    #pos_eigvals, _ = eigsh(A, k=1, tol=1e-2)
+    _, pos_t_mat = lanczos_tridiag(hess_vec_prod, 15, device = params[0].device, dtype = params[0].dtype, matrix_shape=(N,N))
+    pos_eigvals, _ = lanczos_tridiag_to_diag(pos_t_mat)
+    print(pos_eigvals.size())
     maxeig = pos_eigvals[0]
     if verbose and rank == 0: print('max eigenvalue = %f' % maxeig)
 
@@ -134,8 +151,10 @@ def min_max_hessian_eigs(net, dataloader, criterion, rank=0, use_cuda=False, ver
 
     if verbose and rank == 0: print("Rank %d: Computing shifted eigenvalue" % rank)
 
-    A = LinearOperator((N, N), matvec=shifted_hess_vec_prod)
-    neg_eigvals, _ = eigsh(A, k=1, tol=1e-2)
+    #A = LinearOperator((N, N), matvec=shifted_hess_vec_prod)
+    #neg_eigvals, _ = eigsh(A, k=1, tol=1e-2)
+    _, neg_t_mat = lanczos_tridiag(shifted_hess_vec_prod, 50, device = params[0].device, dtype = params[0].dtype, matrix_shape=(N,N))
+    neg_eigvals, _ = lanczos_tridiag_to_diag(neg_t_mat)
     neg_eigvals = neg_eigvals + shift
     mineig = neg_eigvals[0]
     if verbose and rank == 0: print('min eigenvalue = ' + str(mineig))
--- swag/utils.py
+++ swag/utils.py
@@ -13,6 +13,18 @@ def flatten(lst):
     tmp = [i.contiguous().view(-1,1) for i in lst]
     return torch.cat(tmp).view(-1)
 
+def unflatten_like(vector, likeTensorList):
+    # Takes a flat torch.tensor and unflattens it to a list of torch.tensors
+    #    shaped like likeTensorList
+    outList = []
+    i=0
+    for tensor in likeTensorList:
+        #n = module._parameters[name].numel()
+        n = tensor.numel()
+        outList.append(vector[:,i:i+n].view(tensor.shape))
+        i+=n
+    return outList
+    
 def LogSumExp(x,dim=0):
     m,_ = torch.max(x,dim=dim,keepdim=True)
     return m + torch.log((x - m).exp().sum(dim=dim,keepdim=True))
