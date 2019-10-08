d17b086bde2f901800b37002031770d72af96f6d
Wesley Maddox <wm326@cornell.edu>
2019-10-08 17:26:32 -0400

    preresnet83 resize to 32

--- swag/models/preresnet.py
+++ swag/models/preresnet.py
@@ -205,6 +205,7 @@ class PreResNet83:
     kwargs = {"depth": 83}
     transform_train = transforms.Compose(
         [
+            transforms.Resize(32),
             transforms.RandomCrop(32, padding=4),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
@@ -213,6 +214,7 @@ class PreResNet83:
     )
     transform_test = transforms.Compose(
         [
+            transforms.Resize(32),
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
         ]
